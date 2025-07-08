38d4433054809e21d92d2c6e88cba53f13ee45cd
Rakshit Shakya <shakyarakshit1@gmail.com>
2025-07-08 13:17:47 +0530

    03: trpc-setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import { prisma } from "@/lib/db";
+import { getQueryClient, trpc } from "@/trpc/server";
+import {dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Client } from "./client";
+import { Suspense } from "react";
 
 const Home = async () => {
-  const users = await prisma.user.findMany();
-  return (
-    <div>
-      {JSON.stringify(users, null, 2)}
-    </div>
-  );
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.hello.queryOptions({text: "Rakshit PREFETCH"}));
 
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback= {<p>Loading...</p>}>
+      <Client/>
+      </Suspense>
+    </HydrationBoundary>
+  );
 }
  
 export default Home;
