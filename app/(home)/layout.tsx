1e9a32a97842c90b8276d2bb77284bc17c13aab7
Rakshit Shakya <shakyarakshit1@gmail.com>
2026-04-24 21:05:42 +0530

    15: Theme

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -5,7 +5,11 @@ interface Props {
 const Layout = ({ children }: Props) => {
     return (
         <main className="relative flex flex-col min-h-screen">
-    <div className="absolute inset-0 z-0 bg-[radial-gradient(#dadde2_1px,transparent_1px)] bg-[length:16px_16px]" />
+    <div className="absolute inset-0 z-0 
+    bg-[radial-gradient(#dadde2_1px,transparent_1px)] 
+    dark:bg-[radial-gradient(rgba(255,255,255,0.06)_1px,transparent_1px)]
+    bg-[length:18px_18px]" 
+/>
     
     <div className="relative flex-1 flex-col px-4 pb-4 z-10">
         {children}
